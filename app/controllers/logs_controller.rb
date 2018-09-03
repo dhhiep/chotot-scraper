166cbeed1184744097bdd5b2fcfc6322c862ab91
Hiep Dinh <hoanghiepitvnn@gmail.com>
2018-09-04 01:42:24 +0700

    Add Log page

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,4 +1,17 @@
 class LogsController < ApplicationController
+  before_action :load_resource
+
   def index
+    respond_to do |format|
+      format.html
+      format.json { render json: LogDatatable.new(params, view_context: view_context) }
+    end
+  end
+
+  private
+
+  def load_resource
+    return if collection?
+    @log = Log.find(params[:id]) if params[:id].present?
   end
 end
---
+++ app/datatables/log_datatable.rb
@@ -0,0 +1,27 @@
+class LogDatatable < AjaxDatatablesRails::ActiveRecord
+  extend Forwardable
+
+  def view_columns
+    @view_columns ||= {
+      uuid: { source: 'Summary.uuid', cond: :eq },
+      description: { source: 'Summary.description' },
+      created_at: { source: 'Summary.created_at' }
+    }
+  end
+
+  def data
+    records.map do |record|
+      {
+        uuid: record.uuid,
+        description: record.description,
+        created_at: record.created_at.to_display
+      }
+    end
+  end
+
+  private
+
+  def get_raw_records
+    Summary.order(id: :desc)
+  end
+end
