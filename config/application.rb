5feab57c5cf3e4226a8f0ae17e2105ba9f78ceba
Hiep Dinh <hoanghiepitvnn@gmail.com>
2018-09-07 18:55:01 +0700

    fix double quotes

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,6 @@ module ChototScraper
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.time_zone = "Asia/Ho_Chi_Minh"
+    config.time_zone = 'Asia/Ho_Chi_Minh'
   end
 end
